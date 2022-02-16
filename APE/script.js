662c49c9f872dc45cbaa026b1479fb54ec98c70e
Your Name <you@example.com>
2022-02-16 05:06:01 -1200

    (new)

--- APE/script.js
+++ APE/script.js
@@ -1,6 +1,11 @@
 const button = document.querySelector('#connect-btn');
 const modalDiv = document.querySelector('.modal-div');
 const modalClose = document.querySelector('.svg3');
+const Q1 = document.querySelector('#Q1');
+const plusIcon = document.querySelector('#plus-icon');
+const minusIcon = document.querySelector('.minus');
+const subQ1 = document.querySelector('#sub-Q1');
+const panelBlah = document.querySelector('#panel1d-content')
 
 button.addEventListener('click', () => {
     modalDiv.classList.add('modal-active');
@@ -9,3 +14,8 @@ button.addEventListener('click', () => {
 modalClose.addEventListener('click', () => {
     modalDiv.classList.remove('modal-active'); 
 })
+
+Q1.addEventListener('click', () => {
+    
+})
+
